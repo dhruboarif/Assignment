8df522645db5a2f313cdcdffd29e4a0eeb93bbcd
Ariful Islam <ariful48@gmail.com>
2022-02-09 21:17:43 +0600

    comment

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 let products = [
   {
-    name: 'Santa Monica',
+    name: 'T-shirt',
     number: 1995,
     amount: '$10,800',
     due: '12/05/1995',
   },
   {
-    name: 'Stankonia',
+    name: 'One Piece',
     number: 2000,
     amount: '$8,000',
     due: '10/31/2000',
   },
   {
-    name: 'Ocean Avenue',
+    name: 'Salwar Kamiz',
     number: 2003,
     amount: '$9,500',
     due: '07/22/2003',
